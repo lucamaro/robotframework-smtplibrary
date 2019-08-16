d60acd9c8a3504dfebb4a5b262c0aed4e530b1e4
Brandon Wolfe <Wolfe1@gmail.com>
2019-08-16 11:21:19 -0500

    remove these as they have no value but to prevent test runs

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# This file is part of robotframework-smtplibrary.
-# https://github.io/lucamaro/robotframework-smtplibrary
-
-# Licensed under the Apache License 2.0 license:
-# http://www.opensource.org/licenses/Apache License 2.0-license
-# Copyright (c) 2016, Luca Maragnani <luca.maragnani@gmail.com>
